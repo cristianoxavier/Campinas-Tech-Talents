a980b93b81a8d35c531216b59f2f88942b3d0696
Cristiano Xavier <cristiano.xavier@enforcegroup.com.br>
2021-01-12 21:39:15 -0300

    Commit inicial - 12/01
    Nesta aula, fizemos a iniciação com o famoso "Hello World", aprendemos tambem, sobre identação, comentarios, diferenças entre aspas simples, duplas e triplas.

---
+++ Aula_003/comentarios.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# First comment
+print("Hello, Python!") # second comment
+
+name = "Madisetti" # Isso é novamente um comentario, agora dentro da mesma linha // This is again comment
+
+# This is a comment.
+# This is a comment, too.
+# This is a comment, too.
+# I said that already.
+
+
+'''
+This is a multiline
+comment.
+'''
+
+print("Hello, Python!") 
---
+++ Aula_003/linhas.py
@@ -0,0 +1,15 @@
+item_one = ""
+item_two = ""
+item_three = ""
+
+total = item_one + \
+        item_two + \
+        item_three
+
+days = ['Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday']
+
+print(days)
