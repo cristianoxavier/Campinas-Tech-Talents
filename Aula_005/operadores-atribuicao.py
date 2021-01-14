df7903f56d4e06beeb2438c0e811dbc5f3d8d38d
Cristiano Xavier <cristiano.xavier@enforcegroup.com.br>
2021-01-14 00:16:58 -0300

    Adição de explicação sobre operadores logicos e de atribuição.

---
+++ Aula_005/operadores-atribuicao.py
@@ -0,0 +1,5 @@
+= # recebe
++= # Ele soma o operando direito ao operando esquerdo e atribui o resultado ao operando esquerdo.
+-= # Ele subtrai o operando direito ao operando esquerdo e atribui o resultado ao operando esquerdo.
+*= # Ele multiplica o operando direito ao operando esquerdo e atribui o resultado ao operando esquerdo.
+/= # Ele divide o operando direito ao operando esquerdo e atribui o resultado ao operando esquerdo.
